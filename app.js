092e3cb90ece77347aeed5b151d2b9bc948817d0
Robert Rodriguez <RobertRodriguez@MacBookAir.lan>
2025-10-08 20:33:48 -0700

    Frontend update

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ app.use(express.json());
 app.use(cors());
 
 app.use(requestLogger);
+// Temporary for code review
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 
 // Routes
 app.use("/", mainRouter);
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,6 @@
 // const JWT_SECRET = "Secret";
 
 // module.exports = { JWT_SECRET };
-// require('dotenv').config();
 const { JWT_SECRET = "super-strong-secret" } = process.env;
 
 module.exports = {
